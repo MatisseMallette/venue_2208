07a3a5b4f92100913a7ee2290d252abf0f5015ca
Matisse Mallette <matissemallette@gmail.com>
2022-08-24 08:42:03 -0700

    pass i2

--- lib/venue.rb
+++ lib/venue.rb
@@ -5,4 +5,16 @@ class Venue
     @capacity = capacity
     @patrons = []
   end
+
+  def add_patron(patron)
+    patrons << patron
+  end
+
+  def yell_at_patrons
+    yelled_at_patrons = []
+    patrons.each do |patron|
+      yelled_at_patrons << patron.upcase
+    end
+    return yelled_at_patrons
+  end
 end
